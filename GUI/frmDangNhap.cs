f6f1b099312a93224464c061a2c177aae7ca3a91
= <=>
2022-04-24 16:54:12 +0700

    Hao commit DangNhap, 24/04

--- GUI/frmDangNhap.cs
+++ GUI/frmDangNhap.cs
@@ -16,5 +16,12 @@ namespace GUI
         {
             InitializeComponent();
         }
+
+ 
+
+        private void pbxExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
