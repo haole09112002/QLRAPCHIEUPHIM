2a35c385fa69117111bea3be2922766653ebc9cd
Luận Phạm Khắc <102200057@sv1.dut.udn.vn>
2022-04-26 10:26:13 +0700

    Revert "del dang nhap"
    This reverts commit bafac2ab426d7f087f1322d4edd1c2f2baf72f4f.

--- GUI/frmDangNhap.Designer.cs
+++ GUI/frmDangNhap.Designer.cs
@@ -33,6 +33,7 @@
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnDN = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -59,23 +60,22 @@
             this.panel3.Controls.Add(this.pictureBox5);
             this.panel3.Controls.Add(this.pictureBox4);
             this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Controls.Add(this.btnDN);
             this.panel3.Controls.Add(this.comboBox1);
             this.panel3.Controls.Add(this.textBox2);
             this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Location = new System.Drawing.Point(25, 184);
-            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel3.Location = new System.Drawing.Point(22, 147);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(360, 365);
+            this.panel3.Size = new System.Drawing.Size(320, 292);
             this.panel3.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightGray;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(20, 288);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(18, 230);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 45);
+            this.button1.Size = new System.Drawing.Size(114, 36);
             this.button1.TabIndex = 8;
             this.button1.Text = "Làm mới";
             this.button1.UseVisualStyleBackColor = false;
@@ -83,10 +83,9 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::GUI.Properties.Resources.change_user_26px;
-            this.pictureBox5.Location = new System.Drawing.Point(20, 186);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox5.Location = new System.Drawing.Point(18, 149);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(39, 44);
+            this.pictureBox5.Size = new System.Drawing.Size(35, 35);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox5.TabIndex = 7;
             this.pictureBox5.TabStop = false;
@@ -94,10 +93,9 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::GUI.Properties.Resources.password_26px;
-            this.pictureBox4.Location = new System.Drawing.Point(20, 105);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox4.Location = new System.Drawing.Point(18, 84);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(39, 44);
+            this.pictureBox4.Size = new System.Drawing.Size(35, 35);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox4.TabIndex = 6;
             this.pictureBox4.TabStop = false;
@@ -105,13 +103,24 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::GUI.Properties.Resources.male_user_26px;
-            this.pictureBox3.Location = new System.Drawing.Point(20, 20);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(18, 16);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(39, 44);
+            this.pictureBox3.Size = new System.Drawing.Size(35, 35);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
+            // 
+            // btnDN
+            // 
+            this.btnDN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(134)))), ((int)(((byte)(37)))));
+            this.btnDN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDN.Location = new System.Drawing.Point(190, 230);
+            this.btnDN.Name = "btnDN";
+            this.btnDN.Size = new System.Drawing.Size(114, 36);
+            this.btnDN.TabIndex = 3;
+            this.btnDN.Text = "Đăng nhập";
+            this.btnDN.UseVisualStyleBackColor = false;
+            this.btnDN.Click += new System.EventHandler(this.btnDN_Click);
             // 
             // comboBox1
             // 
@@ -120,37 +129,34 @@
             "Admin",
             "QLP",
             "QLVT"});
-            this.comboBox1.Location = new System.Drawing.Point(83, 196);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Location = new System.Drawing.Point(74, 157);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(258, 31);
+            this.comboBox1.Size = new System.Drawing.Size(230, 27);
             this.comboBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(83, 114);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Location = new System.Drawing.Point(74, 91);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(258, 33);
+            this.textBox2.Size = new System.Drawing.Size(230, 28);
             this.textBox2.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(83, 29);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(74, 23);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(258, 33);
+            this.textBox1.Size = new System.Drawing.Size(230, 28);
             this.textBox1.TabIndex = 0;
             // 
-            // label1
+            // pictureBox3
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(229)))), ((int)(((byte)(232)))));
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(119, 105);
+            this.label1.Font = new System.Drawing.Font("VNI 24 Love", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 82);
+            this.label1.Size = new System.Drawing.Size(160, 83);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sign in";
             // 
@@ -161,10 +167,9 @@
             this.panel1.Controls.Add(this.pbxExit);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(441, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Location = new System.Drawing.Point(392, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(459, 625);
+            this.panel1.Size = new System.Drawing.Size(408, 500);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -173,19 +178,28 @@
             this.panel2.Controls.Add(this.pictureBox6);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(24, 34);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Location = new System.Drawing.Point(21, 27);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(412, 574);
+            this.panel2.Size = new System.Drawing.Size(366, 460);
             this.panel2.TabIndex = 16;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox2.Image = global::GUI.Properties.Resources.cinemaiccon;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(394, 500);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::GUI.Properties.Resources.logo;
-            this.pictureBox6.Location = new System.Drawing.Point(163, 4);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox6.Location = new System.Drawing.Point(145, 3);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(96, 109);
+            this.pictureBox6.Size = new System.Drawing.Size(85, 87);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 15;
             this.pictureBox6.TabStop = false;
@@ -193,35 +207,33 @@
             // pbxExit
             // 
             this.pbxExit.Image = global::GUI.Properties.Resources.shutdown_26px;
-            this.pbxExit.Location = new System.Drawing.Point(429, 4);
-            this.pbxExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbxExit.Location = new System.Drawing.Point(381, 3);
             this.pbxExit.Name = "pbxExit";
-            this.pbxExit.Size = new System.Drawing.Size(27, 30);
+            this.pbxExit.Size = new System.Drawing.Size(24, 24);
             this.pbxExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbxExit.TabIndex = 1;
             this.pbxExit.TabStop = false;
+//            this.pbxExit.Click += new System.EventHandler(this.pbxExit_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox2.Image = global::GUI.Properties.Resources.cinemaiccon;
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(443, 625);
+            this.pictureBox2.Size = new System.Drawing.Size(394, 500);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // frmDangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 625);
+            this.ClientSize = new System.Drawing.Size(800, 500);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmDangNhap";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmDangNhap";
@@ -236,6 +248,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxExit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+ //           ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -248,6 +265,7 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnDN;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
