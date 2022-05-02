46d2a9e918cd7e07ab364aeccdc4298e84644d74
= <=>
2022-05-02 17:13:52 +0700

    update program

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -17,7 +17,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmDashboard_AD());
+            Application.Run(new frmDangNhap());
         }
         //Luan vip
         //Hao 
